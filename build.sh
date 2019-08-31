12888bf83327747a8e1fd2a7c232c56955a817ae
Craig Ceremuga <craig@Craigs-MacBook-Air.local>
2019-08-31 09:03:00 -0400

    Renamed binary

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 for GOOS in darwin linux windows; do
     for GOARCH in amd64; do
-        go build -v -o ./bin/sf-package-installer-$GOOS
+        go build -v -o ./bin/rockit-$GOOS
     done
 done
